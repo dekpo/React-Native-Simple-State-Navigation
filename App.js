3c9187c09779a7881d3a52b860f78f040ae2f919
Dekpo <dekpo@me.com>
2021-07-28 15:11:39 +0200

    Added few styles and icons to prepare app for navigation

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Ionicons, FontAwesome } from '@expo/vector-icons';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <View style={styles.header}>
+        <TouchableOpacity>
+          <Ionicons name="md-home" size={24} color="black" />
+        </TouchableOpacity>
+        <Text>Header</Text>
+        <TouchableOpacity>
+          <Ionicons name="md-settings" size={24} color="black" />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.content}>
+        <Text>Content</Text>
+      </View>
+
+      <View style={styles.footer}>
+        <TouchableOpacity>
+          <Ionicons name="md-apps" size={24} color="black" />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Ionicons name="md-logo-instagram" size={24} color="black" />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Ionicons name="md-mail" size={24} color="black" />
+        </TouchableOpacity>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +41,34 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    /* backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center', */
   },
+  header: {
+    height: 90,
+    padding: 20,
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    backgroundColor: '#eee',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  content: {
+    flex: 4,
+    padding: 20,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  footer: {
+    height: 80,
+    padding: 20,
+    borderTopColor: 'black',
+    borderTopWidth: 1,
+    backgroundColor: '#eee',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  }
 });
