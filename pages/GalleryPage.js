dcb7b8c232702887a9b09e478b866244d257e35c
Dekpo <dekpo@me.com>
2021-07-28 17:39:01 +0200

    Added an ImageItem component

--- pages/GalleryPage.js
+++ pages/GalleryPage.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import { StyleSheet,Text, View, FlatList } from 'react-native';
+import { StyleSheet,Text, View, FlatList, Image } from 'react-native';
 
 const getPics = async () => {
   const response = await fetch('https://picsum.photos/v2/list?limit=24');
   const data = await response.json();
   return data;
 }
-
+const ImageItem = ({item}) => {
+  const urlImage = 'https://picsum.photos/id/'+item.id+'/200/200';
+  return (
+    <View>
+      <Image
+      source= {
+        {width: 200,
+        height: 200,
+        uri: urlImage}
+      } />
+    </View>
+  )
+}
 const GalleryPage = () => {
   const [photoList,setPhotoList] = React.useState([]);
 
@@ -20,13 +32,12 @@ const GalleryPage = () => {
   return(
     <View style={styles.content}>
       <Text>Gallery Page</Text>
-      <FlatList 
-      data={photoList}
-      renderItem={ ({item}) => (
-        <ImageItem item={item} />
-      )}
+      <FlatList data={photoList}
+      renderItem={ 
+        ({item}) => (<ImageItem item={item} />)
+      }
       keyExtractor={(item)=>item.id}
-       />
+      numColumns={3} />
     </View>
   )
 
