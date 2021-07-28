7f390807ca36035927cb3296f548654530eca27c
Dekpo <dekpo@me.com>
2021-07-28 17:18:59 +0200

    Added a getPics method to GalleryPage component

--- pages/GalleryPage.js
+++ pages/GalleryPage.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import { StyleSheet,Text, View } from 'react-native';
 
-const GalleryPage = () => (
+const getPics = async () => {
+  const response = await fetch('https://picsum.photos/v2/list?limit=24');
+  const data = await response.json();
+  return data;
+}
+
+const GalleryPage = () => {
+
+  React.useEffect(()=>{
+    getPics().then(data => {
+      console.log(data);
+    })
+  },[]);
+
+  return(
     <View style={styles.content}>
       <Text>Gallery Page</Text>
       <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
@@ -12,7 +26,9 @@ const GalleryPage = () => (
       <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
       <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
     </View>
-  );
+  )
+
+};
   
 export default GalleryPage
 
